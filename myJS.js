416ac53eed6639a11f2829e642aff8afb3511aaf
watanuki469 <75292914+watanuki469@users.noreply.github.com>
2020-12-01 12:09:01 +0700

    watanuki469
    web nhóm 14

---
+++ myJS.js
@@ -0,0 +1,11 @@
+
+// if (confirm("Have you created your account?"))
+// {
+//     alert("hello heo, chào mừng bạn về chuồng  ");
+// }
+
+// else
+// {
+//     alert("vui lòng tạo tài khoản để sử dụng dịch vụ nhé con heo mập mới  O v O ");
+// }
+
